69f3bd8b3c7d0a6e0fdda5c983b5642fca52d261
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2026-02-01 09:50:01 -0400

    Add Game Manager
    Add Game Manager which handles tracking score and timers for the game as well as starting and ending game functions.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("References")]
+    public CriteriaManager criteriaManager;
+    public GuestManager guestManager;
+
+    [Header("Time")]
+    public float startingTime = 300f;
+    public float bonusTime = 3f;
+    public float quickProcessTime = 5f;
+    private float quickProcessTimer;
+    public float gameTimer = 0f;
+
+    [Header("Score")]
+    public long score = 0;
+    public float startingComboMult = 1;
+    public float comboStep = 0.01f;
+    public float comboMult;
+    public float quickProcessPoints = 100;
+    public float basePoints = 100;
+
+    [Header("Sttrikes")]
+    public int maxStrikes = 5;
+    public int currentStrikes = 0;
+
+    private bool gameStarted = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        comboMult = startingComboMult;
+        gameTimer = startingTime;
+        quickProcessTimer = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SystemManager.systemManager.gameState != SystemManager.GameState.Gameplay || !gameStarted) return;
+
+        //process timers
+        if (gameTimer > 0) gameTimer -= Time.deltaTime;
+        else EndGame();
+
+        if (quickProcessTimer > 0) quickProcessTimer -= Time.deltaTime;
+        else if (quickProcessTimer < 0) quickProcessTimer = 0;
+    }
+
+    public void StartGame()
+    {
+        guestManager.GenerateNewGuest();
+        gameStarted = true;
+    }
+
+    public void EndGame()
+    {
+        
+    }
+
+    public void ProcessResult(bool letGuestIn)
+    {
+        if(letGuestIn != criteriaManager.CheckGuest())
+        {
+            IncorrectChoice();
+        }
+        else
+        {
+            CorrectChoice();
+        }
+
+        guestManager.GenerateNewGuest();
+    }
+
+    private void CorrectChoice()
+    {
+        gameTimer += bonusTime;
+        int bonusPoints = (int)(100 * (quickProcessTimer / quickProcessTime));
+        score += (int)((basePoints + bonusPoints) * comboMult);
+
+        comboMult += comboStep;
+        quickProcessTimer = quickProcessTime;
+    }
+
+    private void IncorrectChoice()
+    {
+        currentStrikes++;
+
+        if (currentStrikes >= maxStrikes) EndGame();
+        else
+        {
+            comboMult = startingComboMult;
+            quickProcessTimer = quickProcessTime;
+        }
+    }
+}
